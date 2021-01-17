58375dedd621ca8bf2d7fb5c313a0616ef61e4f6
Pascal Luther <pascal@luther.ch>
2021-01-17 13:57:36 +0100

    chore: Cleanup

--- packages/frontend/store/actions.js
+++ packages/frontend/store/actions.js
@@ -1,6 +1,5 @@
 export default {
   async nuxtServerInit({ dispatch }, ctx) {
-
     /** Get the VERIFIED authUser on the server */
     if (ctx.res && ctx.res.locals && ctx.res.locals.user) {
       const { allClaims: claims, ...authUser } = ctx.res.locals.user
@@ -27,23 +26,13 @@ export default {
     if (authUser && authUser.getIdToken) {
       try {
         const idToken = await authUser.getIdToken(true)
-        console.log("HIER", idToken)
+        console.log('HIER', idToken)
         console.log(this.$axios.defaults)
-        this.$axios.defaults.headers.authorization = `Bearer ${idToken}`;
+        this.$axios.defaults.headers.authorization = `Bearer ${idToken}`
       } catch (e) {
         console.error(e)
       }
     }
     commit('SET_AUTH_USER', { authUser })
-  },
-
-  checkVuexStore(ctx) {
-    if (this.$fire.auth === null) {
-      throw 'Vuex Store example not working - this.$fire.auth cannot be accessed.'
-    }
-
-    alert(
-      'Success. Nuxt-fire Objects can be accessed in store actions via this.$fire___'
-    )
   },
 }
